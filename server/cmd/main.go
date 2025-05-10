1204a7dfdac1d2eba967a079f47eac9584da1c9c
Mazlorii <anucmar24426@gmail.com>
2025-05-10 21:07:20 +0300

    хаб и клиентский интерфейс

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,39 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"server/pkg/packets"
+	"log"
+	"net/http"
+	"server/internal/server"
+	"server/internal/server/clients"
+)
 
-	"google.golang.org/protobuf/proto"
+var (
+	port = flag.Int("port", 8080, "Port to listen on")
 )
 
 func main() {
-	/*
-		packet := &packets.Packet{
-			SenderId: 69,
-			Msg:      packets.NewChat("Hello, world!"),
-		}
+	flag.Parse()
 
-		fmt.Println(packet)
+	hub := server.NewHub()
 
-		data, err := proto.Marshal(packet)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		hub.Serve(clients.NewWebSocketClient, w, r)
+	})
 
-		if err != nil {
-			panic(err)
-		}
+	go hub.Run()
+	addr := fmt.Sprintf(":%d", *port)
 
-		fmt.Println(data)
-	*/
-
-	data := []byte{8, 69, 18, 15, 10, 13, 72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33}
-
-	packet := &packets.Packet{}
-	err := proto.Unmarshal(data, packet)
+	log.Printf("Starting server on %s", addr)
+	err := http.ListenAndServe(addr, nil)
 
 	if err != nil {
-		panic(err)
+		log.Fatalf("ListenAndServe: %v", err)
 	}
-
-	fmt.Println(packet)
-
 }
